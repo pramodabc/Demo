19630d2a2c7660f522a39c53ef1125751566da55
pramodabc <patil0778@gmail.com>
2019-04-09 15:30:36 +0530

    code changes

--- src/main/java/com/scp/sorting/TestSorting.java
+++ src/main/java/com/scp/sorting/TestSorting.java
@@ -5,6 +5,7 @@ public class TestSorting {
 		public static void main(String[] args) {
 			System.out.println("git push");
 			System.out.println("git push 1");
+			System.out.println("");
 			
 		}
 		
